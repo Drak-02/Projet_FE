5075e483b0eb9f372b2eea167e11ac61670f23e6
Drak-02 <keitamister146@gmail.com>
2024-05-04 09:31:31 +0100

    Début d'interface

---
+++ interface_pfe/src/composants/Authentification.js
@@ -0,0 +1,15 @@
+import '../Styles/Authentification.css'
+
+function Authentification(){
+    //Variable 
+        
+    //Traitement
+
+    // Render
+    return (<div>
+
+    </div>)
+
+}
+
+export default Authentification
