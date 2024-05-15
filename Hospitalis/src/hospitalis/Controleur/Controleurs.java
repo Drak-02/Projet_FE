977bd4bb3ff2a30b3886767c47c8eb1f92ddee17
Drak-02 <keitamister146@gmail.com>
2024-05-15 16:45:24 +0100

    Correction interfac

---
+++ Hospitalis/src/hospitalis/Controleur/Controleurs.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hospitalis.Controleur;
+
+/**
+ *
+ * @author badra
+ */
+public class Controleurs {
+    
+}
